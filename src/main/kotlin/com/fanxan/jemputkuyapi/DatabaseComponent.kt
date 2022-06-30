7b664aeef5fe2dc9ee4c3b6ce1b4cfb86ee7641b
Fannan Fauzan Fardhurohman <fannan@jkt1.ebdesk.com>
2022-07-01 00:36:00 +0700

    [system] changing variable for url

--- src/main/kotlin/com/fanxan/jemputkuyapi/DatabaseComponent.kt
+++ src/main/kotlin/com/fanxan/jemputkuyapi/DatabaseComponent.kt
@@ -6,9 +6,6 @@ import org.springframework.stereotype.Component
 
 @Component
 class DatabaseComponent {
-
-    companion object{
-        private const val DB_URL = "mongodb+srv://aej:1234@cluster0.msfnaox.mongodb.net/?retryWrites=true&w=majority"
-    }
+    private val DB_URL = System.getenv("DATABASE_URL")
     val database: MongoClient = KMongo.createClient(DB_URL)
 }
